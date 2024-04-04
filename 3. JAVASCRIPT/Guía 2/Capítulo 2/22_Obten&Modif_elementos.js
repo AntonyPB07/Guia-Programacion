ec253166e1a1bf3f94fb8e0349a9fb183d72bd4b
AntonyPB07 <145941800+AntonyPB07@users.noreply.github.com>
2024-04-03 21:24:31 -0600

    Delete 3. JAVASCRIPT/Guía 2 directory

--- 3. JAVASCRIPT/Guía 2/Capítulo 2/22_Obten&Modif_elementos.js
+++
@@ -1,9 +0,0 @@
-/*
-innerHTML: Propiedad que representa el contenido HTML dentro de un elemento, permitiendo acceder y modificar dicho contenido como una cadena HTML.
-
-innerText: Propiedad que representa el contenido de texto visible dentro de un elemento, sin interpretar las etiquetas HTML.
-
-textContent: Propiedad que representa el contenido textual exacto dentro de un elemento, incluidos los espacios en blanco y las etiquetas HTML.
-
-outerHTML: Propiedad que devuelve una cadena que representa el HTML completo de un elemento, incluyendo su etiqueta de inicio, contenido interno y etiqueta de cierre.
-*/
