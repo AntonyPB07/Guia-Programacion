c1dbb14ce7b77beea55af9bbc0273fae70c3aae7
Antony <antonio.perez0725@gmail.com>
2024-04-16 10:26:58 -0600

    Actualización Listas

--- 8. PYTHON/PYTHON - BÁSICO_2024/07_Listas.py
+++ 8. PYTHON/PYTHON - BÁSICO_2024/07_Listas.py
@@ -41,4 +41,27 @@ print(mi_lista[-3])
 # Desempaquetado de la lista 'mi_lista'
 print('*** DESEMPAQUETADO DE LISTA ***')
 nombre, apellido, edad = mi_lista
+name, *rest = mi_lista
+print(rest)
 print(nombre)
+
+# Dividir listas
+fruits = ['banana', 'orange', 'mango', 'lemon']
+all_fruits1 = fruits[0:4] # it returns all the fruits
+print(all_fruits1)
+# this will also give the same result as the one above
+all_fruits = fruits[0:] # if we don't set where to stop it takes all the rest
+print(all_fruits)
+orange_and_mango = fruits[1:3] # it does not include the first index
+print(orange_and_mango)
+orange_mango_lemon = fruits[1:]
+print(orange_mango_lemon)
+orange_and_lemon = fruits[::2] # here we used a 3rd argument, step. It will take every 2cnd item - ['banana', 'mango']
+print(orange_and_lemon)
+
+# Verificar items en la lista
+fruits = ['banana', 'orange', 'mango', 'lemon']
+does_exist = 'banana' in fruits
+print(does_exist)  # True
+does_exist = 'lime' in fruits
+print(does_exist)  # False
